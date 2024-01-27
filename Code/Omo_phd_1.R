460d21cf382a90a1eda441ffb8ea5b55dd06d2c9
Nosa <osaweemmanulnosa@gmail.com>
2024-01-27 19:11:57 +0100

    modell habitat favourability

--- Code/Omo_phd_1.R
+++ Code/Omo_phd_1.R
@@ -3,9 +3,9 @@ library(lmerTest)
 library(tidyverse)
 library(MASS)
 library(ggfortify)
+library(corrplot)
 
-
-omo_Data <- read.csv("C:\\Users\\HP\\Desktop\\Dr Omoregir\\Phd_Omo\\Updated_Omo_Phd.csv",
+omo_Data <- read.csv("C:\\Users\\HP\\Documents\\Mosquitoes-modelling-\\Data\\Updated_Omo_Phd.csv",
                      stringsAsFactors = TRUE)
 view(omo_Data)
 
@@ -34,19 +34,59 @@ var(omo$Anopheles)
 
 ###  
 
-Anopheles_pred <- glmer(Anopheles ~ scale(Turbidity)* DO + Chloride* Depth+  (1|Ecozones),
-                      data = omo,
-                      family = poisson(link = "log"))
-summary(Anopheles_pred)
-
- 
-Anopheles_pred2 <- glmer(Anopheles ~ scale(Turbidity)* scale(DO) + scale(Cules)+scale(Depth)+
+### WE are done with the anopheles model
+Anopheles_pred1 <- glmer(Anopheles ~ scale(Turbidity)* scale(DO) +
+                          # scale(Cules)+
+                           scale(Aedes)+scale(Depth)+
                            scale(Magnesium) + 
-                           (1|Ecozones),
+                           (1|Ecozones)+ (1|Habitat),
                         data = omo,
                         family = poisson(link = "log"))
-summary(Anopheles_pred2)
+summary(Anopheles_pred1)### this is the best model, thus far!
+
+Anopheles_pred2 <- glmer(Anopheles ~ scale(Turbidity)* scale(DO) +
+                            scale(Cules)+
+                           scale(Aedes)+scale(Depth)+
+                           scale(Magnesium) + 
+                           (1|Ecozones)+ (1|Habitat),
+                         data = omo,
+                         family = poisson(link = "log"))
+summary(Anopheles_pred2)   
 
 
-anova(Anopheles_pred,Anopheles_pred2 )
+anova(Anopheles_pred1,Anopheles_pred2)
+
+################################################################################
+
+Aedes_pred<- glmer(Aedes ~ 
+                      scale(Anopheles) +
+                      scale(Suspended.Solid)+
+                     scale(TDS)+
+                    scale(Chloride)+
+                     scale(Colour)+
+                     scale(BOD)+
+                       (1|Ecozones)+ (1|Habitat),
+                     data = omo,
+                     family = poisson(link = "log"))
+summary(Aedes_pred)
+
+####################################################################################
+
+
+
+          ### Correlation of parameters
+
+cor_matrix <- cor(omo[, c("Turbidity", "pH", "Depth", "Colour", "TDS", "Suspended.Solid",
+                          "Total.Solid", "Conductivity","Chloride", "Alkalinity", 
+                          "Hardness.as.CaCO3", "Phosphate", "Sulphate", "Nitrate",
+                          "DO", "BOD", "Calcium",  "Magnesium", "Anopheles" )], 
+                  method= "spearman")
+corrplot(cor_matrix, 
+         method = "color", 
+         addCoef.col = 'black', 
+         tl.cex = 0.7,      # Variable font size
+         tl.col = 'black',
+         number.cex = 0.5) # font size
+
+
   
